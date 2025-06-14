3bd63521f624b723d4f045cf7511fceea4dcc85a
Hatrickek <halimbayka@outlook.com>
2025-06-15 00:38:16 +0300

    fix project panel resizing

--- OxylusEditor/src/Panels/ProjectPanel.cpp
+++ OxylusEditor/src/Panels/ProjectPanel.cpp
@@ -46,23 +46,21 @@ void ProjectPanel::on_render(vuk::Extent3D extent, vuk::Format format) {
 
   UI::center_next_window();
   if (ImGui::BeginPopupModal("ProjectSelector", nullptr, flags)) {
-    const float x = ImGui::GetContentRegionAvail().x;
-    const float y = ImGui::GetFrameHeight();
-
     const auto banner_size = EditorLayer::get()->engine_banner->get_extent();
+    const float x = static_cast<float>(banner_size.width);
+    const float y = static_cast<float>(ImGui::GetFrameHeight());
 
     const auto& window = App::get()->get_window();
-    const f32 scale = window.get_content_scale();
 
-    UI::image(*EditorLayer::get()->engine_banner,
-              {static_cast<float>(banner_size.width) * scale, static_cast<float>(banner_size.height) * scale});
+    UI::image(*EditorLayer::get()->engine_banner, {x, static_cast<float>(banner_size.height)});
     UI::spacing(2);
     ImGui::SeparatorText("Projects");
     UI::spacing(2);
 
-    if (draw_new_project_panel) {
-      UI::begin_properties();
-      {
+    if (ImGui::BeginChild("##Contents", {}, ImGuiChildFlags_AutoResizeY)) {
+      if (draw_new_project_panel) {
+        UI::begin_properties();
+
         UI::begin_property_grid("Directory", nullptr, false);
         ImGui::SetNextItemWidth(ImGui::GetContentRegionAvail().x * 0.8f);
         ImGui::InputText("##Directory", &new_project_dir, flags);
@@ -91,68 +89,71 @@ void ProjectPanel::on_render(vuk::Extent3D extent, vuk::Format format) {
           });
         }
         UI::end_property_grid();
-      }
-      UI::input_text("Name", &new_project_name);
-      UI::input_text("Asset Directory", &new_project_asset_dir);
-      UI::end_properties();
-      ImGui::Separator();
-      ImGui::SetNextItemWidth(-1);
-      if (ImGui::Button("Create", ImVec2(120, 0))) {
-        new_project(new_project_dir, new_project_name, new_project_asset_dir);
-        visible = false;
-        ImGui::CloseCurrentPopup();
-      }
-      ImGui::SetItemDefaultFocus();
-      ImGui::SameLine();
-      if (ImGui::Button("Cancel", ImVec2(120, 0))) {
-        draw_new_project_panel = false;
-      }
-    } else {
-      const auto projects = EditorConfig::get()->get_recent_projects();
-      for (auto& project : projects) {
-        auto project_name = fs::get_file_name(project);
-        if (ImGui::Button(project_name.c_str(), {x, y})) {
-          load_project_for_editor(project);
+
+        UI::input_text("Name", &new_project_name);
+        UI::input_text("Asset Directory", &new_project_asset_dir);
+        UI::end_properties();
+
+        ImGui::Separator();
+
+        ImGui::SetNextItemWidth(-1);
+        if (ImGui::Button("Create", ImVec2(120, 0))) {
+          new_project(new_project_dir, new_project_name, new_project_asset_dir);
+          visible = false;
+          ImGui::CloseCurrentPopup();
+        }
+        ImGui::SetItemDefaultFocus();
+        ImGui::SameLine();
+        if (ImGui::Button("Cancel", ImVec2(120, 0))) {
+          draw_new_project_panel = false;
+        }
+      } else {
+        const auto projects = EditorConfig::get()->get_recent_projects();
+        for (auto& project : projects) {
+          auto project_name = fs::get_file_name(project);
+          if (ImGui::Button(project_name.c_str(), {x, y})) {
+            load_project_for_editor(project);
+          }
+        }
+
+        ImGui::Separator();
+        if (ImGui::Button(StringUtils::from_char8_t(ICON_MDI_FILE_DOCUMENT " New Project"), {x, y})) {
+          draw_new_project_panel = true;
+        }
+        ImGui::SetNextItemWidth(x);
+        if (ImGui::Button(StringUtils::from_char8_t(ICON_MDI_UPLOAD " Load Project"), {x, y})) {
+          FileDialogFilter dialog_filters[] = {{.name = "Oxylus Project", .pattern = "oxproj"}};
+          window.show_dialog({
+              .kind = DialogKind::OpenFile,
+              .user_data = this,
+              .callback =
+                  [](void* user_data, const c8* const* files, i32) {
+                    auto* usr_data = static_cast<ProjectPanel*>(user_data);
+                    if (!files || !*files) {
+                      return;
+                    }
+
+                    const auto first_path_cstr = *files;
+                    const auto first_path_len = std::strlen(first_path_cstr);
+                    const auto path = std::string(first_path_cstr, first_path_len);
+                    if (!path.empty()) {
+                      usr_data->load_project_for_editor(path);
+                    }
+                  },
+              .title = "Open project...",
+              .default_path = fs::current_path(),
+              .filters = dialog_filters,
+              .multi_select = false,
+          });
+        }
+        UI::align_right(ImVec2(120, 0).x);
+        if (ImGui::Button("Skip", ImVec2(120, 0))) {
+          visible = false;
+          ImGui::CloseCurrentPopup();
         }
       }
-
-      ImGui::Separator();
-      if (ImGui::Button(StringUtils::from_char8_t(ICON_MDI_FILE_DOCUMENT " New Project"), {x, y})) {
-        draw_new_project_panel = true;
-      }
-      ImGui::SetNextItemWidth(x);
-      if (ImGui::Button(StringUtils::from_char8_t(ICON_MDI_UPLOAD " Load Project"), {x, y})) {
-        FileDialogFilter dialog_filters[] = {{.name = "Oxylus Project", .pattern = "oxproj"}};
-        window.show_dialog({
-            .kind = DialogKind::OpenFile,
-            .user_data = this,
-            .callback =
-                [](void* user_data, const c8* const* files, i32) {
-                  auto* usr_data = static_cast<ProjectPanel*>(user_data);
-                  if (!files || !*files) {
-                    return;
-                  }
-
-                  const auto first_path_cstr = *files;
-                  const auto first_path_len = std::strlen(first_path_cstr);
-                  const auto path = std::string(first_path_cstr, first_path_len);
-                  if (!path.empty()) {
-                    usr_data->load_project_for_editor(path);
-                  }
-                },
-            .title = "Open project...",
-            .default_path = fs::current_path(),
-            .filters = dialog_filters,
-            .multi_select = false,
-        });
-      }
-      UI::align_right(ImVec2(120, 0).x);
-      if (ImGui::Button("Skip", ImVec2(120, 0))) {
-        visible = false;
-        ImGui::CloseCurrentPopup();
-      }
+      ImGui::EndChild();
     }
-    UI::spacing(4);
     ImGui::EndPopup();
   }
 }
